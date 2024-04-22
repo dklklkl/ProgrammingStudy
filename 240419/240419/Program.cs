a633c73f85a93a9f47518f78a6ae05256fb70f1c
dklklkl <psh98416@gmail.com>
2024-04-22 14:17:04 +0900

    feat: 6.if 제어문

--- 240419/240419/Program.cs
+++ 240419/240419/Program.cs
@@ -1,137 +1,72 @@
-﻿//5.컬렉션 연습2
+﻿//6. IF 제어문
 
-/*Stack<int> numbers = new Stack<int>();
-numbers.Push(10); //값을 쌓아 올린다.
-numbers.Push(100);
-numbers.Push(1000);
+/*using System.Diagnostics.Metrics;
 
-foreach (int number in numbers)
+bool isActive = true;
+
+if(isActive)
 {
-    Console.WriteLine(number);
+    Console.WriteLine("작동중입니다.");
+}
+else
+{
+    Console.WriteLine("정지상태입니다.");
 }
 
-Console.WriteLine("-----------");
+int number = 5;
 
-int peekNumber = numbers.Peek();
-Console.WriteLine(peekNumber);
+if (number == 0)        Console.WriteLine("");
+else if (number != 0)   Console.WriteLine("");
+else if (number > 0)    Console.WriteLine("");
+else if (number < 0)    Console.WriteLine("");
+else if (number >= 0)   Console.WriteLine("");
+else if (number <= 0)   Console.WriteLine("");
 
-bool isExist = numbers.Contains(100);
-if (isExist)
-    Console.WriteLine("100이 존재합니다.");
+//논리연산자 AND(&&), OR(||), NOT(!)
+if (number == 0 && isActive == true)        Console.WriteLine("");
+else if (number == 0 || isActive == false)  Console.WriteLine("");
+else if (isActive)                          Console.WriteLine("");*/
 
-int value = numbers.Pop(); //쌓아 올린 값을 순서대로 꺼낸다.
-Console.WriteLine(value);
+/*번호에 따른 시퀀스 작동
+int status = 0;
 
-value = numbers.Pop(); //쌓아 올린 값을 순서대로 꺼낸다.
-Console.WriteLine(value);
-
-value = numbers.Pop(); //쌓아 올린 값을 순서대로 꺼낸다.
-Console.WriteLine(value);*/
-
-/*실습1. 작업(파워포인트)를 되돌리기 상태로 만들어주는 예제
-//1. 특정 횟수만큼 파워포인트 각 작업을 반복해서 넣고(ReadLine)
-//2. (넣는 과정이 끝나면) 1번 입력(되돌리기 버튼)을 누를 경우, 되돌리기 작업의 이름을 출력
-//3. 되돌리기 작업과 현재 진행된 작업의 이름을 stack에 쌓인 순서대로 출력
-//menu>graph>x-setting>y-setting>insert>change x-setting>change y-setting>delete
-
-Console.WriteLine("-----------");
-
-Stack<string> works = new Stack<string>();
-works.Push("menu");
-works.Push("graph");
-works.Push("x-setting");
-works.Push("y-setting");
-works.Push("insert");
-works.Push("change x-setting");
-works.Push("change y-setting");
-works.Push("delete");
-
-foreach (string work in works)
+switch(status)
 {
-    Console.WriteLine(work);
-}
-
-Console.WriteLine("-----------");
-
-Console.WriteLine("명령을 입력하세요.");
-
-string input = Console.ReadLine();
-
-if (input == "1")
-{
-    string undoValue = works.Pop();
-    Console.WriteLine(undoValue);
-
-    Console.WriteLine("-----------");
-
-    foreach (string work in works)
-    {
-        Console.WriteLine(work);
-    }
+    case 0:
+        Console.WriteLine("0번 상태입니다.");
+        break;
+    case 1:
+        Console.WriteLine("1번 상태입니다.");
+        break;
+    case 2:
+        Console.WriteLine("2번 상태입니다.");
+        break;
 }*/
 
-/*Console.WriteLine("-----------");
+/*실습3 PLC 5초 딜레이 신호등 if문 또는 switch case문으로 작성해 봅니다.
+ * 조건 : status
+ * M10(0), M11(1), M12(2) -> Console.WriteLine("황색 전구 ON")
+ * Timer -> Console.WriteLine("5초 Timer ON")*/
 
-Queue<string> waitingPeople = new Queue<string>();
-waitingPeople.Enqueue("짱구");
-waitingPeople.Enqueue("철수");
-waitingPeople.Enqueue("맹구");
 
-foreach (string people in waitingPeople)
+for(int i = 0; i<3; i++)
 {
-    Console.WriteLine(people);
+    switch (i)
+    {
+        case 0:
+            Console.WriteLine("GREEN ON");
+            Console.WriteLine("5초 Timer ON");
+            break;
+        case 1:
+            Console.WriteLine("YELLOW ON");
+            Console.WriteLine("5초 Timer ON");
+            break;
+        case 2:
+            Console.WriteLine("RED ON");
+            Console.WriteLine("5초 Timer ON");
+            break;
+    }
 }
 
-string name = waitingPeople.Dequeue();
-Console.WriteLine(name);
 
-name = waitingPeople.Dequeue();
-Console.WriteLine(name);
 
-name = waitingPeople.Dequeue();
-Console.WriteLine(name);*/
-
-/*실습2 - Queue
- * 도서관 대출자 대기열
- * 1. 대출자 대기열에 맞춰 대출자의 이름과 도서의 이름을 대기열에 등록
- * 2. 도서관 직원이 대기열에 맞춰 대출목록에 업데이트
- * 
- * 입출력 예시
- * "대출자 이름을 입력해주세요."
- * 짱구
- * "도서 권수를 입력해주세요."
- * 2
- * "대출을 위한 도서 목록을 입력해주세요."
- * 퓨처셀프
- * "대출을 위한 도서 목록을 입력해주세요."
- * 삼국지
- * 
- * "Queue에 들어갈 내용 -> 이름 : 짱구 \n 도서1 : 퓨처셀프 \n 도서2 : 삼국지
- * 
- * "도서가 등록되었습니다."
- * 이름 : 짱구
- * 도서1 : 퓨처셀프
- * 도서2 : 삼국지
- */
-
-Queue<string> libraryQueue = new Queue<string>();
-string totalinfo = "";
-
-Console.WriteLine("대출자 이름을 입력해주세요.");
-string name = Console.ReadLine();
-Console.WriteLine("도서 권수를 입력해주세요.");
-string booknumber = Console.ReadLine();
-int count = int.Parse(booknumber);
-
-totalinfo = name + "\n" + count + "\n";
-
-for (int i = 0; i < count; i++)
-{
-    Console.WriteLine("도서 목록을 입력해주세요.");
-    string bookname = Console.ReadLine();
-    totalinfo += bookname + "\n";
-}
-
-Console.WriteLine("입력된 내용은 아래와 같습니다.");
-Console.WriteLine(totalinfo);
-libraryQueue.Enqueue(totalinfo);
--- 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
+++ 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240419")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e9956ea2cc6bae3a8c77a5b3257507a9d2c8114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e002e627e5384cd9b29350a54727aecc4c0dbcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("240419")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240419")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
